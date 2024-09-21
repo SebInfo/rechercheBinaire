b2102738f050d37147f91b329ad8ab3882f68864
SebInfo <contact@inion.info>
2024-09-21 08:34:13 +0200

    Ajout de la fonction recherche linéraire afin de comparer les deux algos

--- rechercheBinaire.py
+++ rechercheBinaire.py
@@ -1,3 +1,11 @@
+def recherche_lineaire(tableau, cible):
+    for i in range(len(tableau)):
+        print("#", end="")
+        if tableau[i] == cible:
+            return i  # La cible a été trouvée à l'indice i
+    return -1  # La cible n'a pas été trouvée
+
+
 def recherche_binaire(arr, target):
     min, max = 0, len(arr) - 1
     while min <= max:
@@ -12,4 +20,5 @@ def recherche_binaire(arr, target):
     return -1  # Si la valeur n'est pas trouvée
 
 tableau = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31]
-print(recherche_binaire(tableau, 7))  # Sortie : 7
+print(recherche_binaire(tableau, 4))  # Sortie : 7
+print(recherche_lineaire(tableau, 4))
